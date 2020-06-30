0fcc46aa0a782a27ecd21f52cbfa81eed38d9642
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2020-06-30 22:50:45 +0530

    "Apod Model created for receiving data from network"

--- app/src/main/java/nasa/photo/oftheday/data/model/ApodModel.kt
+++ app/src/main/java/nasa/photo/oftheday/data/model/ApodModel.kt
@@ -1,12 +1,39 @@
 package nasa.photo.oftheday.data.model
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 data class ApodModel(
+
+    @SerializedName("copyright")
+    @Expose
     val copyright: String,
+
+    @SerializedName("date")
+    @Expose
     val date: String,
+
+    @SerializedName("explanation")
+    @Expose
     val explanation: String,
+
+    @SerializedName("hdurl")
+    @Expose
     val hdurl: String,
+
+    @SerializedName("media_type")
+    @Expose
     val media_type: String,
+
+    @SerializedName("service_version")
+    @Expose
     val service_version: String,
+
+    @SerializedName("title")
+    @Expose
     val title: String,
+
+    @SerializedName("url")
+    @Expose
     val url: String
 )
