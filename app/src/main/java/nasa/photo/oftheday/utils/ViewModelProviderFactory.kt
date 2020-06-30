8a0e405d9dd6efb43a02f3466e7147d9314cfb54
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2020-06-30 22:13:17 +0530

    "ViewModelProviderFactory generic class created to help in creating View Model class with any number of dependencies"

---
+++ app/src/main/java/nasa/photo/oftheday/utils/ViewModelProviderFactory.kt
@@ -0,0 +1,31 @@
+package nasa.photo.oftheday.utils
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+import kotlin.reflect.KClass
+
+/**
+ * Created by Pallab Banerjee on 6/30/2020.
+ */
+
+/**
+* A ViewModel provider factory class , for creating VieModel class with any number of dependencies in its constructor
+* */
+
+class ViewModelProviderFactory<T: ViewModel> (
+
+    private val kClass: KClass<T>,
+    private val creator : () -> T
+
+) : ViewModelProvider.NewInstanceFactory() {
+
+
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IllegalArgumentException::class)
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(kClass.java)) return creator as T
+        throw (IllegalArgumentException("Unknown class name"))
+    }
+
+}
