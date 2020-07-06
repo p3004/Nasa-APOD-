f42db72c9f99254b23ec41439bf3f7c0f80c2901
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2020-07-06 17:08:05 +0530

    "Test passed : UI test for MainActivity done to check if the text views are displaying expected data on server response 200"

--- app/src/androidTest/java/nasa/photo/oftheday/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/nasa/photo/oftheday/ui/main/MainActivityTest.kt
@@ -2,8 +2,7 @@ package nasa.photo.oftheday.ui.main
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -38,7 +37,16 @@ class MainActivityTest {
         onView(withId(R.id.tvMainDescription)).check(matches(isDisplayed()))
         onView(withId(R.id.ivMainApodBackgroundImage)).check(matches(isDisplayed()))
         onView(withId(R.id.ivMainDatePicker)).check(matches(isDisplayed()))
-        
+    }
+
+    @Test
+    fun givenServerResponse200_checkIfTextViewsHasExpectedTexts(){
+
+        onView(withId(R.id.tvMainTitleText)).check(matches(withText("M43: Dust, Gas, and Stars in the Orion Nebula")))
+        onView(withId(R.id.tvMainDescription)).check(matches(withText("Unspeakable beauty and unimaginable bedlam can be found together in the Orion Nebula Arguably the most famous of all astronomy nebulas, the Great Nebula in Orion is an immense interstellar molecular cloud only 1500 light-years away.  In the featured deep image shown in assigned colors, the part of the nebula's center known as M43 is shown as taken by the Hubble Space Telescope. The Great Nebula in Orion can be found with the unaided eye near the easily identifiable belt of three stars in the popular constellation Orion.  The entire Orion Nebula," +
+                " including both M42 and M43 spans about 40 light years and is located in the same spiral arm of our Galaxy as the Sun.")))
+        onView(withId(R.id.tvMainDate)).check(matches(withText("2020-07-06")))
+
     }
 
 
